88ff2a6558eb537f91b12a8520ca84d323337363
omar star <obarakat12@gmail.com>
2021-10-16 19:39:01 +0300

    refactor(app): start server async

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const tasksRouter = require("./routes/tasks.route");
+const connectDB = require("./db/connect");
 //middleware
 app.use(express.json());
 //routes
@@ -9,4 +10,14 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.use(process.env.ROUTE_PREFIX + "/tasks", tasksRouter);
 
 const port = process.env.PORT | 3000;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () =>
+      console.log(`Example app listening on port ${port}!`)
+    );
+  } catch (error) {
+    console.log("error connecting DB: ", error);
+  }
+};
+start();
